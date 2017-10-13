12d158538b3ac90cc2ef20d42628a916862b7057
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2017-10-12 21:04:27 -0700

    Aesthetic Improvements
    Addresses Enhancement #1.
    
    Added spooky bat spawner to create random bats on the screen.
    Days until Halloween will be recalculated periodically so the page will still properly display the days remaining even if the page is left open for a long time.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
-var daysUntilHalloween = getDaysUntilHalloween();
+function calculateDaysLeft(){
+	var daysUntilHalloween = getDaysUntilHalloween();
 
-if (daysUntilHalloween === 0){
-	document.getElementById("spooky").innerText = "It's Halloween!";
+	if (daysUntilHalloween === 0){
+		document.getElementById("spooky").innerText = "It's Halloween!";
+	}
+	else if (daysUntilHalloween === 1){
+		document.getElementById("spooky").innerText = "Tomorrow is Halloween!";
+	}
+	else{
+		document.getElementById("spooky").innerText = "There are "+daysUntilHalloween+" days until Halloween.";
+	}
 }
-else if (daysUntilHalloween === 1){
-	document.getElementById("spooky").innerText = "Tomorrow is Halloween!";
-}
-else{
-	document.getElementById("spooky").innerText = "There are "+daysUntilHalloween+" days until Halloween.";
-}
-
-function getDaysUntilHalloween(){
-	var today = new Date();
-	var year = today.getFullYear();
-	if (today.getMonth() > 9)
-		year += 1;
-	var halloween = new Date(year, 9, 30);
-	var elapsedTime = halloween-today;
-	return Math.floor(elapsedTime/86400000);
-}
+calculateDaysLeft();
+setInterval(calculateDaysLeft, 60);
 
 var splashText = [
 	"Best Witches",
@@ -32,6 +26,84 @@ var splashText = [
 ];
 
 document.getElementById("splashText").innerText = randFromList(splashText);
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var batImage = loadImage("img/bat.png");
+var bats = [];
+var nextBatSpawn = 0;
+
+canvasAutoFullScreen();
+draw();
+
+
+
+function draw(){
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	requestAnimationFrame(draw);
+	if (nextBatSpawn < Date.now())
+		spawnBat();
+	handleBats();
+}
+
+function spawnBat(){
+	nextBatSpawn = Date.now()+rand(500, 1500);
+	bats.push({
+		x: rand(50, canvas.width-50),
+		y: rand(50, canvas.height-50),
+		degrees: rand(-45, 45),
+		rotationDirection: (rand(0, 100) < 50 ? 2 : -2),
+		deathTime: Date.now()+rand(1000, 5000),
+		scale: .2,
+	});
+}
+
+function handleBats(){
+	for (var i = bats.length-1; i >= 0; i--){
+		var bat = bats[i];
+		if (bat.deathTime < Date.now()){
+			bats.splice(i, 1);
+			continue;
+		}
+		drawImage(batImage, bat.x, bat.y, bat.scale, bat.degrees);
+		bat.degrees += bat.rotationDirection;
+		bat.scale += .003;
+		if (bat.degrees < -45 || bat.degrees > 45)
+			bat.rotationDirection *= -1;
+	}
+}
+
+function loadImage(src){
+	var image = new Image();
+	image.src = src;
+	return image;
+}
+
+function drawImage(image, x, y, scale, degrees){
+	context.save();
+	context.translate(x, y);
+	context.rotate(degrees*Math.PI/180);
+	context.drawImage(image, -image.width*scale/2, -image.height*scale/2, image.width*scale, image.height*scale);
+	context.restore();
+}
+
+function canvasAutoFullScreen(){
+	window.onresize = function(){
+		canvas.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+	}
+	window.onresize();
+}
+
+function getDaysUntilHalloween(){
+	var today = new Date();
+	var year = today.getFullYear();
+	if (today.getMonth() > 9)
+		year += 1;
+	var halloween = new Date(year, 9, 30);
+	var elapsedTime = halloween-today;
+	return Math.floor(elapsedTime/86400000);
+}
 
 function rand(min, max){
 	min = parseInt(min, 10);
