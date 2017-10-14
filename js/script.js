67634514e36be2f87b3ab833e8d04b6cee53b775
Dan Horsch <anokahorsch@gmail.com>
2017-10-14 00:18:10 -0500

    added my images, updated html/css/javascript with spooky code

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ else {
 // Display message
 
 spooky.innerText = message;
+
+// fly the witch
+function witchFly() {
+	witch = document.getElementById("witch");
+	pos = 2000;
+	var id = setInterval(fly,4);
+
+	function fly() {
+		if(pos == -80) {
+			clearInterval(id);
+		}
+		else {
+			pos--;
+			witch.style.left = pos + "px";
+		}
+	}
+}
