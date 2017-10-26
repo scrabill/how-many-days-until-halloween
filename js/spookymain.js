1cd3053bb5ed1e25de4651fea773d45ce25bd9f0
Gautam Mandsorwale <mandsorwalegautam@gmail.com>
2017-10-26 19:03:57 +0530

    Added Spooky text
    When black cats prowl and pumpkins gleam, May luck be yours on Halloween

--- js/spookymain.js
+++ js/spookymain.js
@@ -35,7 +35,8 @@ var splashText = [
 	"Creep it Real",
 	"Drink Up Witches!",
 	"If You’ve Got It, Haunt it",
-	"Don’t Make Me Get The Flying Monkeys"
+	"Don’t Make Me Get The Flying Monkeys",
+	"When black cats prowl and pumpkins gleam, May luck be yours on Halloween"
 ];
 
 
