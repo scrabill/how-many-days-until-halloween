c5848ec581fd865fa26d7297eac07f335e9e430f
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2018-10-08 21:30:08 +0530

    Add jack-o-lantern emoji

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ const halloween = moment("31-10", "DD-MM"); // Next Halloween
 const message = getMessage(halloween.diff(today, "days")); // Calculate difference
 
 // Display message
-spooky.innerText = `${message} 👻`;
+spooky.innerText = `${message} 👻🎃`;
 
 // Map of allowed keys for Konami Code
 const allowedKeys = {
