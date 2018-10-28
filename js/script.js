80e4415f31e10602a1473c076f30179ea35d0ac0
Vivek Verma <rohitpandey486@gmail.com>
2018-10-28 12:18:41 +0530

    Timer for Halloween is updated

--- js/script.js
+++ js/script.js
@@ -7,9 +7,6 @@ const factElement = document.querySelector('#fact');
 
 // Array of Halloween facts
 // Source: https://www.womansday.com/life/g485/15-fascinating-halloween-facts-124464/
-// Source: https://www.huffingtonpost.in/entry/halloween-weird-facts_n_5948456
-// Source: https://www.factretriever.com/halloween-facts
-
 const facts = [
     'California, Illinois, Michigan, New York, Ohio, and Pennsylvania produce the majority of pumpkins for Halloween – over 1 billion pounds a year!',
     'The name Jack-O-Lantern first originated from an Irish folktale about a man named Stingy Jack who tricked the Devil over and over again. When Jack died, he was forced to walk the Earth with only a carved-out turnip and burning coal to help light his way.',
@@ -26,28 +23,6 @@ const facts = [
     'Trunk-or-Treating was created as a safer alternative to Trick-or-Treating in 2000.',
     'Halloween ranks as the sixth most popular card-giving holiday, with 20 million cards sent each year.',
     'In 2017, the most popular pet costumes were pumpkins, hot dogs, and a tie between a dog, lion, and pirate.',
-    'Candy Corn was originally known as "chicken feed"',
-    'Samhainophobia is the fear of Halloween.',
-    'Halloween is thought to have originated around 4000 B.C., which means Halloween has been around for over 6,000 years.',
-    'The first Jack O’Lanterns were actually made from turnips.',
-    "Some animal shelters won't allow the adoption of black cats around Halloween for fear they'll be sacrificed.",
-    'In a few American towns, Halloween was originally referred to as \"Cabbage Night.\"',
-    'The first Jack O\’Lanterns were actually made from turnips.',
-    'Samhainophobia is the fear of Halloween.',
-    'The largest pumpkin ever measured was grown by Norm Craven, who broke the world record in 1993 with a 836 lb. pumpkin.',
-    'Trick-or-treating evolved from the ancient Celtic tradition of putting out treats and food to placate spirits who roamed the streets at Samhain, a sacred festival that marked the end of the Celtic calendar year.',
-    'The first known mention of trick-or-treating in print in North America occurred in 1927 in Blackie, Alberta, Canada.',
-    'Ireland is typically believed to be the birthplace of Halloween.',
-    'Scarecrows, a popular Halloween fixture, symbolize the ancient agricultural roots of the holiday.',
-    'Halloween has variously been called All Hallows’ Eve, Witches Night, Lamswool, Snap-Apple Night, Samhaim, and Summer’s End.',
-    'In 2010, 72.2% of those surveyed by the National Retail Federation will hand out candy, 46.3% will carve a pumpkin, 20.8% will visit a haunted house, and 11.5% will dress up their pets.',
-    'The Village Halloween parade in New York City is the largest Halloween parade in the United States. The parade includes 50,000 participants and draws over 2 million spectators.',
-    'Comedian John Evans once quipped: “What do you get if you divide the circumference of a jack-o’-lantern by its diameter? Pumpkin π.”',
-    'Boston, Massachusetts, holds the record for the most Jack O’Lanterns lit at once (30,128).',
-    'Both Salem, Massachusetts, and Anoka, Minnesota, are the self-proclaimed Halloween capitals of the world.',
-    'Halloween is thought to have originated around 4000 B.C., which means Halloween has been around for over 6,000 years.',
-    'According to tradition, if a person wears his or her clothes inside out and then walks backwards on Halloween, he or she will see a witch at midnight.',
-    'Because the movie Halloween (1978) was on such a tight budget, they had to use the cheapest mask they could find for the character Michael Meyers, which turned out to be a William Shatner Star Trek mask. Shatner initially didn’t know the mask was in his likeness, but when he found out years later, he said he was honored.',
 ];
 
 // Display message and fact
@@ -58,33 +33,60 @@ const colors = {
     spooky: "#ada48f"
 };
 
-const getMessage = days => {
-    if (days < 0) {
-        return `Halloween was ${-days} days ago.`;
-    }
+function getTimeRemaining(){
+    const date = "2018-10-31";
+    const time = "00:00:00";
+    const timeAndDate = moment(date+' '+time);
+    const destination  = timeAndDate._i;
+    const curr_date = moment().format('YYYY-MM-DD HH:mm:ss');
+    const duration = moment(destination).diff(moment(curr_date))  
+    var seconds = Math.floor((duration / 1000) % 60);
+    var minutes = Math.floor((duration / 1000 / 60) % 60);
+    var hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(duration / (1000 * 60 * 60 * 24));
+    return {
+      'total': duration,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
 
-    if (days === 0) {
-        return "It's Halloween! Stay spooky!";
-    }
-
-    if (days === 1) {
-        return "Halloween is tomorrow! Be afraid, be very afraid!";
-    }
-
-    return `There are ${days} days until Halloween.`;
-};
-
-const today = moment().startOf('day'); // Today
-const halloween = moment("31-10", "DD-MM"); // Next Halloween
-const message = getMessage(halloween.diff(today, "days")); // Calculate difference
-
-// Display message
-spooky.innerHTML = `<div>${message} 👻🎃</div>
+function initializeClock(id) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      var t = getTimeRemaining();
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        $("#clockdiv").empty();
+        $(".old_msg").empty();
+        const message = "It's Halloween! Stay spooky!";
+        clearInterval(timeinterval);
+        spooky.innerHTML = `<div>${message} 👻🎃</div>
                     <div id="outerCostumeBox">
                     <button id="costumeButton">🦇</button>
                     <div id="costumeContent">Got a costume yet?
-                     <br>Click on the bat for a random idea!</div>
+                    <br>Click on the bat for a random idea!</div>
                     </div>`;
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+initializeClock('clockdiv');
 
 // Map of allowed keys for Konami Code
 const allowedKeys = {
@@ -141,40 +143,3 @@ const doKonami = () => {
         }
     });
 };
-
-
-// Script for Random Costume Picker
-
-const costumes = [
-    ["Napolen Dynamite", "https://i.pinimg.com/originals/26/2a/f4/262af40e4592db053fd8613773e3f5b0.jpg"],
-    ["Castaway","https://www.pinterest.ca/pin/16958936081976494/"],
-    ["a 404error", "https://www.instagram.com/p/7QsQ9Vhj9_/?utm_source=ig_embed"],
-    ["50 Shades of Grey", "https://i.pinimg.com/474x/9a/45/42/9a454265d6a6e59ba609831a1b7c7028--so-funny-funny-stuff.jpg"],
-    ["Octocat", "https://twitter.com/Bint_alBeatz/status/793178052494815233"],
-    ["The Shining", "https://i.pinimg.com/originals/21/e3/a9/21e3a9859795366b7cc1b7aa3c8ed598.jpg"]
-    ["Tamagotchi", "https://www.pinterest.ca/pin/192810427770341992/"],
-    ["Clippy","https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F83%2F37%2F31%2F8337315668b57bcbf4fcb82a4fde2a99.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F518758450798995072%2F&docid=zgMyyDogGc7eJM&tbnid=Ex7IH-SdFvQoaM%3A&vet=10ahUKEwjXjKbr0_ndAhWL1FkKHZg5DfYQMwg7KAEwAQ..i&w=650&h=975&safe=off&client=firefox-b-ab&bih=781&biw=1440&q=halloween%20clippy&ved=0ahUKEwjXjKbr0_ndAhWL1FkKHZg5DfYQMwg7KAEwAQ&iact=mrc&uact=8"],
-    ["Mark Zuckerberg", "https://costumewall.com/dress-like-mark-zuckerberg/"],
-    ["a stick figure", "https://twitter.com/CarlForrest/status/1050448521353207808"]
-  ];
-
-  //Assign DOM elements
-  let costumeButton = document.getElementById("costumeButton");
-  let costumeContent = document.getElementById("costumeContent");
-
-  //Pick a random costume
-  const costumeIs = () => {
-    let numCostumes = costumes.length;
-    let randomNumber = Math.floor( ( Math.random() * numCostumes ) );
-    return costumes[randomNumber];
-  }
-
-  //Display costume on page with link.
-  const newCostume = () => {
-    costume = costumeIs();
-    costumeContent.innerHTML = `Got a costume yet?<br>
-                                You could be: <a href="${costume[1]}" target="_blank">${costume[0]}</a>`;
-  };
-
-  //Add event listener on 'click'
-  costumeButton.addEventListener('click', newCostume, false);
