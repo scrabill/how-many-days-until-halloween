bc45644ef17b513f229552da63ae5104fa5cbe12
Scriptle <44384393+Scriptle@users.noreply.github.com>
2018-10-28 19:47:06 +0000

    Added Costume Idea

--- js/script.js
+++ js/script.js
@@ -184,7 +184,8 @@ const costumes = [
     ["Clippy","https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F83%2F37%2F31%2F8337315668b57bcbf4fcb82a4fde2a99.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F518758450798995072%2F&docid=zgMyyDogGc7eJM&tbnid=Ex7IH-SdFvQoaM%3A&vet=10ahUKEwjXjKbr0_ndAhWL1FkKHZg5DfYQMwg7KAEwAQ..i&w=650&h=975&safe=off&client=firefox-b-ab&bih=781&biw=1440&q=halloween%20clippy&ved=0ahUKEwjXjKbr0_ndAhWL1FkKHZg5DfYQMwg7KAEwAQ&iact=mrc&uact=8"],
     ["Mark Zuckerberg", "https://costumewall.com/dress-like-mark-zuckerberg/"],
     ["a stick figure", "https://twitter.com/CarlForrest/status/1050448521353207808"],
-    ["a Selfie", "https://twitter.com/hellofelicia14/status/1055155960384290816"]
+    ["a Selfie", "https://twitter.com/hellofelicia14/status/1055155960384290816"],
+    ["Blue Screen of Death", "https://i.redd.it/gvq357z64iu11.jpg"]
   ];
 
   //Assign DOM elements
