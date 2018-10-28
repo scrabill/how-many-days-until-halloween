8588b7e08d4dd4120d3916e4571ee1eea22aac47
Stefan Ivic <nightwalkerkg@gmail.com>
2018-10-28 10:47:19 +0100

    Add a new halloween fact

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ const facts = [
     'Halloween is thought to have originated around 4000 B.C., which means Halloween has been around for over 6,000 years.',
     'According to tradition, if a person wears his or her clothes inside out and then walks backwards on Halloween, he or she will see a witch at midnight.',
     'Because the movie Halloween (1978) was on such a tight budget, they had to use the cheapest mask they could find for the character Michael Meyers, which turned out to be a William Shatner Star Trek mask. Shatner initially didn’t know the mask was in his likeness, but when he found out years later, he said he was honored.',
+    'According to ancient Roman records, tribes located in today’s Germany and France traditionally wore costumes of animal heads and skins to connect to spirits of the dead.',
 ];
 
 // Display message and fact
