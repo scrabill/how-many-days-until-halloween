526bcd4cf4ac1b2b10b0f7027c75a89cf9e98933
scrabill <shannon.crabill@gmail.com>
2018-10-28 01:46:16 -0400

    Removes still from countdown message

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ const getMessage = days => {
         return "Halloween is tomorrow! Be afraid, be very afraid!";
     }
 
-    return `There are still ${days} days until Halloween.`;
+    return `There are ${days} days until Halloween.`;
 };
 
 const today = moment().startOf('day'); // Today
