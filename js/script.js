99adf9e3e4c3a01804c6d16225f5767ba55512d7
crobinson661 <44535946+crobinson661@users.noreply.github.com>
2018-10-28 09:29:43 -0400

    Added additional facts
    My first submission to an open source project! Feedback welcome!

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const factElement = document.querySelector('#fact');
 const facts = [
     'California, Illinois, Michigan, New York, Ohio, and Pennsylvania produce the majority of pumpkins for Halloween – over 1 billion pounds a year!',
     'The name Jack-O-Lantern first originated from an Irish folktale about a man named Stingy Jack who tricked the Devil over and over again. When Jack died, he was forced to walk the Earth with only a carved-out turnip and burning coal to help light his way.',
+    '99% of all pumpkins sold are used as Jack-O-Lanterns for Halloween.',
     'The Plymouth Colony Pilgrims started the superstition about black cats.',
     'Tootsie Rolls, Smarties, and candy corn were among the most hated candies in 2017.',
     "Reese's Peanut Butter Cups were 2017’s most popular Halloween candy.",
@@ -20,6 +21,7 @@ const facts = [
     'Silly String is banned in Hollywood on Halloween.',
     'In 2014, the total estimated cost of all Halloween costumes was $2.8 billion.',
     'Candy Corn was created in 1898 and 35 million pounds of it are made every year.',
+    'Candy corn has been made with the same recipe by the Jelly Belly Candy Company since 1898.',
     'Halloween in the second largest consumer holiday in the United States. Consumers spent about $7 billion in 2015.',
     "Guinness World Records has named the Haunted Cave in Lewisburg, Ohio as the world's longest haunted house. It measures 3,564 feet long and is located 80 feet below ground in an abandoned mine.",
     'Harry Houdini died on Halloween in 1926.',
