7e6dd6ce9ee5281c3f18ebddc5740afa55642461
Ted Jacobs <doldrone@bellsouth.net>
2018-10-28 21:46:01 -0400

    Add only before days until and exclamation to end

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ const getMessage = days => {
         return "Halloween is tomorrow! Be afraid, be very afraid!";
     }
 
-    return `There are ${days} days until Halloween.`;
+    return `There are only ${days} days until Halloween!`;
 };
 
 const today = moment().startOf('day'); // Today
