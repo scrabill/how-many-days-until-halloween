d6bbe9dcf47a45b751dcf7525b2f531f1b705c07
Justin Schweppe <jschwepp@gmail.com>
2018-10-29 11:31:31 -0500

    Update js/script.js
    Added Costume Idea.

--- js/script.js
+++ js/script.js
@@ -201,7 +201,8 @@ const costumes = [
     ["Oh yeah!", "https://www.pinterest.com/pin/271975264971863827/visual-search/?x=9&y=9&w=282&h=342"],
     ["Ecce homo", "https://theheureka.com/wp-content/uploads/2012/10/3Ts6B.jpeg"],
     ["Blessing in Disguise", "https://www.instagram.com/p/BpcONbEAUnu/?hl=en"],
-    ["Blue Screen of Death", "https://i.redd.it/gvq357z64iu11.jpg"]
+    ["Blue Screen of Death", "https://i.redd.it/gvq357z64iu11.jpg"],
+    ["Shredded Girl with Balloon by Bansky", "https://twitter.com/WorldOfWard/status/1056284833327001600"]
   ];
 
   //Assign DOM elements
