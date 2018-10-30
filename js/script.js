aa63ec909c4b31666d5841fe99c5834d3929d5ec
Victor Apolonio <apolonio@protonmail.com>
2018-10-30 19:53:12 -0300

    inserting another fact about the halloween

--- js/script.js
+++ js/script.js
@@ -81,7 +81,8 @@ const facts = [
     'Children are more than twice as likely to be killed in a pedestrian/car accident on Halloween than on any other night.',
     'Pumpkin carving in bulk is a popular Guinness World Record. The proud Halloween enthusiasts of Highwood, Illinois took the record in 2011 with 30,919 simultaneously lit Jack-O-Lanterns.',
     'Studies have shown that Halloween actually makes kids act more evil!',
-    'A full moon on Halloween is extremely rare.'
+    'A full moon on Halloween is extremely rare.',
+    'According to legend, if you put your clothes on inside out and walk backwards at Halloween, you’ll see a witch at midnight.'
 ];
 
 // Display message and fact
