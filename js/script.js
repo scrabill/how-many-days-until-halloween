7e656eaa90565a536c28d0bf18fb7f6cf91b695d
Dmytro Kharchenko <30443685+dkharch@users.noreply.github.com>
2018-10-31 01:26:06 +0200

    Added new facts

--- js/script.js
+++ js/script.js
@@ -81,6 +81,8 @@ const facts = [
     'Children are more than twice as likely to be killed in a pedestrian/car accident on Halloween than on any other night.',
     'Pumpkin carving in bulk is a popular Guinness World Record. The proud Halloween enthusiasts of Highwood, Illinois took the record in 2011 with 30,919 simultaneously lit Jack-O-Lanterns.',
     'Studies have shown that Halloween actually makes kids act more evil!',
+    'There\'s a $1,000 fine for using or selling Silly String in Hollywood on Halloween.',
+    'Halloween is the second highest grossing commercial holiday after Christmas.',
     'A full moon on Halloween is extremely rare.'
 ];
 
