4993ac21fe83aca96c278a5b2954ae4f332299a8
veronicorn <vronicalee@gmail.com>
2018-11-01 18:09:43 -0400

    added a couple of facts

--- js/script.js
+++ js/script.js
@@ -110,7 +110,9 @@ const facts = [
     'Many myths explain that spotting a spider on Halloween is actually a loved one watching over you!',
     'The UNICEF orange collection boxes are iconic in Canada, with Canadians continuing to donate on average $3 million every Halloween.',
     'On average, a licking machine designed by engineering students at Purdue needed 364 licks to reach the centre of a tootsie pop.',
-    'In Brazil, the date of Halloween is called witch\`s day'
+    'In Brazil, the date of Halloween is called witch\`s day',
+    'The popular movie, Hocus Pocus, was almost called Halloween House instead!',
+    'Magician Harry Houdini died on Halloween in 1926.'
 ];
 
 // Display message and fact
