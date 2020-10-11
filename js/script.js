e5bd1d4291fc102d46c037c3589c3c56e8d24f67
scrabill <shannon.crabill@gmail.com>
2020-10-10 20:31:33 -0400

    Adds days until to title tag

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
     const date = document.querySelector("#date")
+    const title = document.querySelector("title")
 
     const now = new Date()
     const halloween = new Date(`October 31, ${now.getFullYear()} 00:00:00`)
@@ -9,5 +10,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const daysUntil = Math.abs(Math.ceil(timeUntil / (1000 * 60 * 60 * 24)))
 
     date.innerHTML = daysUntil
+    title.innerHTML = `${daysUntil} Days Until Halloween!`
 
 });
