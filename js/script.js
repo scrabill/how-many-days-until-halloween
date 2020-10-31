26ea59b99a3777b4b385307ccc8d2383fa7069e5
scrabill <shannon.crabill@gmail.com>
2020-10-30 23:32:38 -0400

    Adds characters to download for custom font

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const now = new Date()
     const halloween = new Date(`October 31, ${now.getFullYear()} 00:00:00`)
     const timeUntil = halloween.getTime() - now.getTime()
-    // const daysUntil = Math.abs(Math.ceil(timeUntil / (1000 * 60 * 60 * 24)))
-    const daysUntil = 0
+    const daysUntil = Math.abs(Math.ceil(timeUntil / (1000 * 60 * 60 * 24)))
 
     if (daysUntil == 0) {
         h1.innerHTML = "Today is Halloween! Eat, drink and be scary!"
