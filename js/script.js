00dcded5113cf1acf2094f0d6f4ba27e18444325
scrabill <shannon.crabill@gmail.com>
2020-10-30 23:32:02 -0400

    Adds messaging for when today is halloween

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     const date = document.querySelector("#date")
     const title = document.querySelector("title")
+    const h1 = document.querySelector("h1")
 
     const now = new Date()
     const halloween = new Date(`October 31, ${now.getFullYear()} 00:00:00`)
     const timeUntil = halloween.getTime() - now.getTime()
-    const daysUntil = Math.abs(Math.ceil(timeUntil / (1000 * 60 * 60 * 24)))
+    // const daysUntil = Math.abs(Math.ceil(timeUntil / (1000 * 60 * 60 * 24)))
+    const daysUntil = 0
 
-    date.innerHTML = daysUntil
-    title.innerHTML = `${daysUntil} Days Until Halloween!`
+    if (daysUntil == 0) {
+        h1.innerHTML = "Today is Halloween! Eat, drink and be scary!"
+        title.innerHTML = "Ahhhh! Today is Halloween!"
+    } else {
+        date.innerHTML = daysUntil
+        title.innerHTML = `${daysUntil} Days Until Halloween!`
+    }
+
 
 });
